5ab044fef1e2521c44d391f365476ebd17dd7f27
AmosTLOU <didala2009@163.com>
2022-02-17 11:29:14 -0700

    Refine Gallery

--- Assets/Scripts/PhotoGallery.cs
+++ Assets/Scripts/PhotoGallery.cs
@@ -6,25 +6,43 @@ using UnityEngine.UI;
 using System.IO;
 
 // Use DulNode structure to view and scroll photos
+//[HideInInspector]
+//public class DuLNode
+//{
+//    public string StrName { get; set; }
+//    public bool HasClue { get; set; }
+//    public string ClueName { get; set; }
+//    public Phase PhaseBelongTo { get; set; }
+//    public Vector3 ViewPos { get; set; }
+//    public DuLNode Prev { get; set; }
+//    public DuLNode Next { get; set; }
+
+//    public DuLNode(string i_strName, Phase i_phase){
+//        StrName = i_strName;
+//        HasClue = false;
+//        ClueName = "";
+//        PhaseBelongTo = i_phase;
+//        ViewPos = Vector3.zero;
+//        Prev = null;
+//        Next = null;
+//    }
+//}
+
 [HideInInspector]
-public class DuLNode
+public class Photo
 {
-    public string StrName { get; set; }
+    public string FileName { get; set; }
     public bool HasClue { get; set; }
     public string ClueName { get; set; }
     public Phase PhaseBelongTo { get; set; }
     public Vector3 ViewPos { get; set; }
-    public DuLNode Prev { get; set; }
-    public DuLNode Next { get; set; }
-
-    public DuLNode(string i_strName, Phase i_phase){
-        StrName = i_strName;
+    public Photo(string i_FileName)
+    {
+        FileName = i_FileName;
         HasClue = false;
         ClueName = "";
-        PhaseBelongTo = i_phase;
+        PhaseBelongTo = Phase.NullPhase;
         ViewPos = Vector3.zero;
-        Prev = null;
-        Next = null;
     }
 }
 
@@ -37,12 +55,12 @@ public class PhotoGallery : MonoBehaviour
     public GameObject HintDetails;
 
     GameManager m_gameManager;
-
+    List<Photo> m_photos;
     float m_scaleX;
     float m_scaleY;
-    DuLNode m_headNode;
-    DuLNode m_tailNode;
-    DuLNode m_curNode;
+    //DuLNode m_headNode;
+    //DuLNode m_tailNode;
+    //DuLNode m_curNode;
     int m_cntPhoto;
     int m_curIndex;
     string m_pathPhotos;
@@ -51,6 +69,7 @@ public class PhotoGallery : MonoBehaviour
     private void Start()
     {
         m_gameManager = FindObjectOfType<GameManager>();
+        m_photos = new List<Photo>();
 
         TextIndexOfPhoto.text = "";
         HintDetails.SetActive(false);
@@ -58,8 +77,8 @@ public class PhotoGallery : MonoBehaviour
         // since the viewPos is normalized, we need the scale to put the red cirlce on the right position
         m_scaleX = (Screen.width + ImageDisplay.GetComponent<RectTransform>().rect.width)/2;
         m_scaleY = (Screen.height + ImageDisplay.GetComponent<RectTransform>().rect.height)/2;
-        m_headNode = null;
-        m_tailNode = null;
+        //m_headNode = null;
+        //m_tailNode = null;
         m_cntPhoto = 0;
         m_curIndex = -1;
 
@@ -68,7 +87,7 @@ public class PhotoGallery : MonoBehaviour
         if (Directory.Exists(m_pathPhotos)) 
         {
             Debug.Log("Clearing Old Photos.");
-            Directory.Delete(m_pathPhotos); 
+            Directory.Delete(m_pathPhotos, true); 
         }
         Debug.Log("Creating a new gallery.");
         Directory.CreateDirectory(m_pathPhotos);
@@ -79,34 +98,21 @@ public class PhotoGallery : MonoBehaviour
     {
         //string strDateAndTime = System.DateTime.Now.ToString();
         ScreenCapture.CaptureScreenshot(m_pathPhotos + m_cntPhoto + ".png");
-        if (m_headNode == null)
-        {
-            m_headNode = new DuLNode(m_cntPhoto.ToString(), Phase.NullPhase);
-            m_tailNode = m_headNode;
-            m_headNode.Next = m_tailNode;
-            m_tailNode.Prev = m_headNode;
-        }
-        else
-        {
-            m_tailNode.Next = new DuLNode(m_cntPhoto.ToString(), Phase.NullPhase);
-            m_tailNode.Next.Prev = m_tailNode;
-            m_tailNode = m_tailNode.Next;
-            m_tailNode.Next = m_headNode;
-            m_headNode.Prev = m_tailNode;
-        }
+        m_photos.Add(new Photo(m_cntPhoto.ToString()));
+        Debug.Log("Capture");
         m_cntPhoto++;
     }
 
     // Show a full picture
-    public void Show(DuLNode node, int index)
+    public void Show(int index)
     {
         TextHintMessage.gameObject.SetActive(false);
         ImageRedCircle.gameObject.SetActive(false);
         HintDetails.SetActive(false);
-        if (node != null && 0 <= index)
+        if (0 < m_cntPhoto && 0 <= index)
         {
             byte[] bytes;
-            bytes = System.IO.File.ReadAllBytes(m_pathPhotos + node.StrName + ".png");
+            bytes = System.IO.File.ReadAllBytes(m_pathPhotos + m_photos[index].FileName + ".png");
             Texture2D textureLoad = new Texture2D(1, 1);
             textureLoad.LoadImage(bytes);
             if (textureLoad)
@@ -120,61 +126,75 @@ public class PhotoGallery : MonoBehaviour
                 return;
             }
             TextIndexOfPhoto.text = (index + 1) + " / " + m_cntPhoto;
-            if (node.HasClue && node.PhaseBelongTo <= m_gameManager.GetPhase())
+            if (m_photos[index].HasClue && m_photos[index].PhaseBelongTo <= m_gameManager.GetPhase())
             {
                 TextHintMessage.gameObject.SetActive(true);
                 ImageRedCircle.gameObject.SetActive(true);
-                ImageRedCircle.rectTransform.anchoredPosition = new Vector2((2f * node.ViewPos.x - 1f) * m_scaleX, (2f * node.ViewPos.y - 1f) * m_scaleY);
+                ImageRedCircle.rectTransform.anchoredPosition = new Vector2((2f * m_photos[index].ViewPos.x - 1f) * m_scaleX, (2f * m_photos[index].ViewPos.y - 1f) * m_scaleY);
             }            
         }
     }
 
     public void EnterGallery()
     {
-        m_curNode = m_tailNode;
+        //m_curNode = m_tailNode;
+
+        if (m_cntPhoto <= 0)
+            return;
         m_curIndex = m_cntPhoto-1;
-        Show(m_curNode, m_curIndex);
+        Show(m_curIndex);
     }
 
     public void ShowNextPhoto()
     {
-        if (m_curNode == null)
+        //if (m_curNode == null)
+        //    return;
+
+        if (m_cntPhoto <= 0)
             return;
         m_curIndex++;
         m_curIndex %= m_cntPhoto;
-        m_curNode = m_curNode.Next;
-        Show(m_curNode, m_curIndex);
+        Show(m_curIndex);
     }
 
     public void ShowPrevPhoto()
     {
-        if (m_curNode == null)
+        //if (m_curNode == null)
+        //    return;
+
+        if (m_cntPhoto <= 0)
             return;
         m_curIndex--;
         if (m_curIndex < 0)
             m_curIndex = m_cntPhoto - 1;
-        m_curNode = m_curNode.Prev;
-        Show(m_curNode, m_curIndex);
+        Show(m_curIndex);
     }
 
     public void AddPromptToPhoto(Vector3 viewPos, string clueName, Phase phaseBelongTo)
     {
-        if(m_tailNode == null)
+        //if(m_tailNode == null)
+        //{
+        //    Debug.Log("Clue is captured, but fail to take a photo.");
+        //    return;
+        //}
+
+        if (m_cntPhoto <= 0)
         {
             Debug.Log("Clue is captured, but fail to take a photo.");
             return;
         }
-        m_tailNode.HasClue = true;
-        m_tailNode.ViewPos = viewPos;
-        m_tailNode.ClueName = clueName;
-        m_tailNode.PhaseBelongTo = phaseBelongTo;
+        int lastIndex = m_cntPhoto - 1;
+        m_photos[lastIndex].HasClue = true;
+        m_photos[lastIndex].ViewPos = viewPos;
+        m_photos[lastIndex].ClueName = clueName;
+        m_photos[lastIndex].PhaseBelongTo = phaseBelongTo;
     }
 
     public void OpenOrCloseDetails()
     {
-        if (m_curNode.HasClue)
+        if (m_photos[m_curIndex].HasClue)
         {
-            if(m_curNode.ClueName == "Clue_Poster")
+            if(m_photos[m_curIndex].ClueName == "Clue_Poster")
                 HintDetails.SetActive(!HintDetails.activeInHierarchy);
         }
     }
