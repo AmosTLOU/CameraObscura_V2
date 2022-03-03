fce3a731bac06aeed474d28ced3eab9be0d9b332
Anna Chen <53270829+anna-chestnut@users.noreply.github.com>
2022-03-03 14:24:47 -0700

    Add Evidence Board Scene & Script

---
+++ Assets/Scripts/EvidenceBoard.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CharacterInfo = Characters.Data.CharacterInfo;
+
+public class EvidenceBoard : Core.SingletonBehaviour<EvidenceBoard>
+{
+
+    List<Photo> m_clues;
+    List<CharacterInfo> m_characters;
+
+    public void AddClues(Photo photo)
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
