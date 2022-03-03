5ef0894cf36bb8400b579ef099788a503ecd0561
Ejaz Ahmed <ejaz.mohammed@umail.utah.edu>
2022-03-03 13:42:03 -0700

    testing scriptable object lifecycle

--- Assets/Scripts/Characters/CharacterManager.cs
+++ Assets/Scripts/Characters/CharacterManager.cs
@@ -1,8 +1,23 @@
-﻿using Core;
+﻿using System;
+using System.Collections.Generic;
+using Core;
 using UnityEngine;
+using CharacterInfo = Characters.Data.CharacterInfo;
 
 namespace Characters {
     public class CharacterManager : SingletonBehaviour<CharacterManager> {
-        
+        [SerializeField] private List<CharacterInfo> _characters = new List<CharacterInfo>();
+
+        private void Start(){
+            _characters[0].AddNote("hello there");
+        }
+
+        private static int c = 1;
+        private void Update(){
+            if (c++ % 100 == 0){
+                c -= 100;
+                _characters[0].AddNote($"hello there {_characters[0].Notes.Count}");
+            }
+        }
     }
 }
