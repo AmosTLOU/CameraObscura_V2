3f9b6390fcd69ed63fd4e1255a30a9793c68f8b5
Anna Chen <53270829+anna-chestnut@users.noreply.github.com>
2022-04-07 14:12:39 -0600

    Chef Animation setting done

--- Assets/Scripts/Characters/Chef.cs
+++ Assets/Scripts/Characters/Chef.cs
@@ -3,6 +3,49 @@ using System;
 
 namespace Characters {
     public class Chef : BaseCharacter {
-        
+
+        public bool Chop;
+        public bool SlowRun;
+        public bool Idle;
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            if (Chop)
+            {
+                Chop = false;
+                _animator.SetTrigger("Chop");
+            }
+
+            if (SlowRun)
+            {
+                SlowRun = false;
+                _animator.SetTrigger("SlowRun");
+            }
+
+            if (Idle)
+            {
+                Idle = false;
+                _animator.SetTrigger("Idle");
+            }
+        }
+
+        public void ChopAnimation()
+        {
+            _animator.SetTrigger("Chop");
+        }
+
+        public void SlowRunAnimation()
+        {
+            _animator.SetTrigger("SlowRun");
+        }
+
+        public void IdleAnimation()
+        {
+            _animator.SetTrigger("Idle");
+        }
     }
 }
