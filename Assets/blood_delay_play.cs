4c4bba385585fbf97dab2223e8713e3caf529d6d
Otakudesuka <67305202+Otakudesuka@users.noreply.github.com>
2022-04-17 16:18:28 -0600

    purple feather particles

---
+++ Assets/blood_delay_play.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blood_delay_play : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float time;
+    float timedelay;
+
+    public ParticleSystem blood;
+    void Start()
+    {
+        blood = GetComponent<ParticleSystem>();
+        time = 0f;
+        timedelay = 4.0f;
+        //blood.Stop();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //StartCoroutine(Example());
+        time = time + 1f * Time.deltaTime;
+        if (time >= timedelay)
+        {
+            time = 0f;
+            blood.Play();
+        }
+    }
+
+}
+
+
+//   IEnumerator Example()
+//    {
+//        blood.Play();
+//        yield return new WaitForSeconds(2);
+//        blood.Play();
+//    }
+//}
