f7daf43c693c00a80e9634671ab7a17f9c8b53b4
Anna Chen <53270829+anna-chestnut@users.noreply.github.com>
2022-04-19 12:06:49 -0600

    windows

--- Assets/Scripts/World/RoomWindow.cs
+++ Assets/Scripts/World/RoomWindow.cs
@@ -2,9 +2,21 @@
 
 namespace World {
     public class RoomWindow : MonoBehaviour {
-        
+
+        protected Animator _animator;
+        public bool remainClose;
+        protected void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            if (remainClose)
+                _animator.SetTrigger("idle_close");
+        }
         public void UpdateState(bool open){
-            
+
+            if (open)
+                _animator.SetTrigger("open");
+            else
+                _animator.SetTrigger("close");
         }
     }
 }
