23c710484f9f03dbe092be7b107733fba62f3713
AmosTLOU <didala2009@163.com>
2022-04-21 12:31:02 -0600

    Start to incorporate menu function into the system

--- Assets/Scripts/UI/MenuInputManager.cs
+++ Assets/Scripts/UI/MenuInputManager.cs
@@ -6,9 +6,15 @@ using UnityEngine.UI;
 
 public class MenuInputManager : SingletonBehaviour<MenuInputManager>
 {
+    public GameObject CanvasHUD;
+    public Animator AnimatorClickToStart;
+    public RawImage RawImageCamera;
+    public RenderTexture renderTextureCamera;
+    public Camera SecondCamera;
+
     //public Texture2D TextureCursor;
     public Image ImageCursor;
-    public GameObject[] Layers;
+    public GameObject[] Layers;   
 
     private int m_layer;
 
@@ -16,6 +22,10 @@ public class MenuInputManager : SingletonBehaviour<MenuInputManager>
     // Start is called before the first frame update
     void Start()
     {
+        CanvasHUD.gameObject.SetActive(false);
+        SecondCamera.gameObject.SetActive(true);
+        RawImageCamera.gameObject.SetActive(false);
+
         ShowLayer(0);
 
         //Vector2 centerOfCursor = new Vector2(TextureCursor.width * 0.5f, TextureCursor.height * 0.5f);
@@ -48,5 +58,29 @@ public class MenuInputManager : SingletonBehaviour<MenuInputManager>
                 Layers[i].SetActive(false);
             }            
         }
+    }
+
+    IEnumerator EnlargeScreen()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        RawImageCamera.gameObject.SetActive(true);
+        Vector2 ScreenSize = new Vector2(Screen.width, Screen.height);
+        while (RawImageCamera.rectTransform.sizeDelta != ScreenSize)
+        {
+            RawImageCamera.texture = renderTextureCamera;
+            RawImageCamera.rectTransform.sizeDelta = Vector2.MoveTowards(RawImageCamera.rectTransform.sizeDelta, ScreenSize, Time.deltaTime * 650);
+            Vector2 v2_centerOfPicture = new Vector2(RawImageCamera.rectTransform.anchoredPosition.x, RawImageCamera.rectTransform.anchoredPosition.y);
+            RawImageCamera.rectTransform.anchoredPosition = Vector2.MoveTowards(v2_centerOfPicture, Vector2.zero, Time.deltaTime * 65);
+            yield return null;
+        }
+        CanvasHUD.SetActive(true);
+        SecondCamera.gameObject.SetActive(false);
+        gameObject.SetActive(false);
+    }
+    public void LoadGame()
+    {
+        AnimatorClickToStart.SetBool("Start", true);
+        StartCoroutine(EnlargeScreen());
     }
 }
